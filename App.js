bddb7d6725e205bdac4f85124b521939b8363cc6
胡安冉 <huanran@huanrandeMacBook-Air.local>
2024-01-16 13:45:28 -0800

    added a variable for app name and use it in the welcome message

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "mobile App"
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffb',
     alignItems: 'center',
     justifyContent: 'center',
   },
